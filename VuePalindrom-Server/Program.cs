b59db2465878dd6148f0f4063bafff607557ac3f
Alexander Selling <alexander@icontrolapp.se>
2019-07-17 14:38:36 +0200

    added kostiantyns suggestion as notes

--- VuePalindrom-Server/Program.cs
+++ VuePalindrom-Server/Program.cs
@@ -6,14 +6,31 @@ namespace VuePalindrom_Server
 {
     class Program
     {
+        /*
+         Create a handler which is going to create a session and return its id.
+
+          - View front end requests Starcounter URL with `accept application/json` header.
+           - Starcounter responds with JSON view-model and `X-Location` header to establish WebSocket connection.
+           - Vue reads the headers and establishes WebSocket connection by the specified location.
+         */
         static void Main()
         {
             DefaultStarcounterBootstrapper.Start(new Startup());
 
-            Handle.GET("/VuePalindrom-Server/session", (Request request) =>
+            Handle.GET("/VuePalindrom-Server/", (Request request) =>
             {
+
                 var session = Session.Ensure();
-                return session.SessionId;
+
+                var resp = new Response()
+                {
+                    //StatusCode = 200,
+                    //ContentType = "text/html",
+                    // X-Location = "...ws server?.."
+                    //Body = session.SessionId
+                };
+
+                return resp;
             });
         }
     }
